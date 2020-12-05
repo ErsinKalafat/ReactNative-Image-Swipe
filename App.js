f6901b338b061d28cf47d8e63929cbbd6c6cb9b2
Ersin <ersin0105@gmail.com>
2020-12-05 22:39:50 +0300

    Reducer, Provider, Store yapısı oluşturuldu.

--- App.js
+++ App.js
@@ -1,21 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React , {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default  App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import {createStore} from "redux";
+import {Provider} from "react-redux"
+
+const initialState = {
+    durum: 'İlk Değer'
+};
+
+const reducer = (state = initialState, action) => {
+    if (action.type === 'setDurum') {
+        return Object.assign({},state, {durum: 'yenilenenDeğer'});
+    }
+    return state;
+};
+
+const store = createStore(reducer);
+
+export default class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+              <View style={styles.container}>
+                <Text>Merhaba Akinon!</Text>
+                <StatusBar style="auto"/>
+              </View>
+            </Provider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
