544201b48f5f8ea181a757bb7150f6edf9b39081
Ersin <ersin0105@gmail.com>
2020-12-05 22:22:45 +0300

    React Native uygulaması oluşturularak Redux kuruldu.

---
+++ application/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const Main = () => {
+    return (
+        <View>
+            <Text>
+                Hello
+            </Text>
+        <View/>
+    )
+
+}
+
+export default Main;
